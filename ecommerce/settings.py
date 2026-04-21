72ade1931c03de70d3484c0b0e72a4e0b666777e
Prashant Sonune <prashantssonune07@gmail.com>
2026-04-21 19:03:10 +0530

    Fix: disable ssl_require for Render internal PostgreSQL connection

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -69,7 +69,7 @@ if DATABASE_URL:
         'default': dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True,
+            ssl_require=False,  # Internal Render connections don't use SSL
         )
     }
 else:
