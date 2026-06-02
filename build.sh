afa6b0be2fc82912b0d5bcf33e45f89b138e8aba
prashantsonune07 <prashantsonune07@gmail.com>
2026-06-02 21:13:45 +0530

    auto create superuser on render

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'admin@example.com', 'Admin@1234')" | python manage.py shell
 python manage.py shell -c "from ecommerceapp.models import Product; Product.objects.all().delete(); print('Old products cleared')"
 python manage.py loaddata products.json
 echo "Products loaded successfully"
